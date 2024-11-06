d51eb0b7a78ef581c1c8b0b580b0b10b605b02a6
Ely Dev <57238178+ElyDeveloper@users.noreply.github.com>
2024-11-05 19:18:36 -0600

    [FIX] configuracion adicional para build y deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'http://localhost:4321', // Agrega esta línea con tu dominio
   integrations: [tailwind()],
   vite: {
     envPrefix: 'PUBLIC_'
