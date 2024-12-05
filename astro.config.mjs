574c00f7224a868e98e86aed518346d681b51e9d
Ely Dev <57238178+ElyDeveloper@users.noreply.github.com>
2024-12-04 20:19:32 -0600

    Integracion de react para multiidioma

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
 
 export default defineConfig({
-  site: 'http://localhost:4321', // Agrega esta línea con tu dominio
-  integrations: [tailwind()],
+  site: 'http://localhost:4321',
+  integrations: [
+    tailwind(),
+    react() // Añadimos la integración de React
+  ],
   vite: {
     envPrefix: 'PUBLIC_'
   }
