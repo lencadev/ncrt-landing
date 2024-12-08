68ed76a8c647e27431234f11f86e282e42f75ed4
Ely Dev <57238178+ElyDeveloper@users.noreply.github.com>
2024-12-07 18:24:34 -0600

    Cambiar site

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'http://localhost:4321',
+  // site: 'http://localhost:4321',
+  site: 'https://ncrtlanding.netlify.app',
   integrations: [
     tailwind(),
   ],
