3e752a30f75ae5c8d83a786cdb0f789e833336d3
Ely Dev <57238178+ElyDeveloper@users.noreply.github.com>
2024-12-07 18:27:29 -0600

    Agregar pages soporte español

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -1,12 +1,18 @@
 import { type APIRoute } from 'astro';
 const pages = [
-  { url: '', priority: '1.0', changefreq: 'weekly' },
-  { url: '/who-we-are', priority: '0.8', changefreq: 'monthly' },
-  { url: '/what-we-do', priority: '0.8', changefreq: 'monthly' },
-  { url: '/territory', priority: '0.8', changefreq: 'monthly' },
-  { url: '/contact', priority: '0.8', changefreq: 'monthly' }
+  // English pages
+  { url: '/en', priority: '1.0', changefreq: 'weekly' },
+  { url: '/en/who-we-are', priority: '0.8', changefreq: 'monthly' },
+  { url: '/en/what-we-do', priority: '0.8', changefreq: 'monthly' },
+  { url: '/en/territory', priority: '0.8', changefreq: 'monthly' },
+  { url: '/en/contact', priority: '0.8', changefreq: 'monthly' },
+  // Spanish pages
+  { url: '/es', priority: '1.0', changefreq: 'weekly' },
+  { url: '/es/who-we-are', priority: '0.8', changefreq: 'monthly' },
+  { url: '/es/what-we-do', priority: '0.8', changefreq: 'monthly' },
+  { url: '/es/territory', priority: '0.8', changefreq: 'monthly' },
+  { url: '/es/contact', priority: '0.8', changefreq: 'monthly' }
 ];
-
 export const GET: APIRoute = async ({ site }) => {
   if (!site) throw new Error('site is undefined');
 
